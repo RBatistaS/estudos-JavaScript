002de33981b1a5e3bf8676a47f635a55bc179266
RBatistaS <99196725+RBatistaS@users.noreply.github.com>
2022-10-13 22:23:28 -0300

    Update ex008.js

--- JavaScript/curso-guanabara/aula11/ex008.js
+++ JavaScript/curso-guanabara/aula11/ex008.js
@@ -1,9 +1,9 @@
-var vel = 60.5
+var vel = 6.5
 
 console.log(`A velocidade do seu carro é ${vel} Km/h`)
 
-if (vel > 60) {
+if (vel > 60) { //Condição simples!
     console.log(`Você ultrapassou a velocidade permitada. MULTADO!`)
 }
 
-console.log(`Dirija sempre usado cinto de segurança`)
+console.log(`Dirija sempre usado cinto de segurança!`)
